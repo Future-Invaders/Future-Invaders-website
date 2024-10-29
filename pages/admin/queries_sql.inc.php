8143bea3e30288668bf801f121e8ecd46d76d5cc
Éric Bisceglia <bisceglia.eric@gmail.com>
2024-10-29 14:54:44 +0100

    Arsenals: Change data structure

--- pages/admin/queries_sql.inc.php
+++ pages/admin/queries_sql.inc.php
@@ -843,14 +843,16 @@ if($last_query < 12)
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Arsenals, arsenal compositions, arsenal tags, arsenal factions, arsenal images, arsenal difficulties, game formats
+// Arsenals, arsenal compositions, arsenal tags, arsenal factions,arsenal difficulties, game formats
 
 if($last_query < 13)
 {
   sql_create_table('arsenals');
   sql_create_field('arsenals', 'uuid', 'VARCHAR(36) NOT NULL', 'id');
   sql_create_field('arsenals', 'fk_releases', 'INT UNSIGNED NOT NULL DEFAULT 0', 'uuid');
-  sql_create_field('arsenals', 'fk_formats', 'INT UNSIGNED NOT NULL DEFAULT 0', 'fk_releases');
+  sql_create_field('arsenals', 'fk_images_en', 'INT UNSIGNED NOT NULL DEFAULT 0', 'fk_releases');
+  sql_create_field('arsenals', 'fk_images_fr', 'INT UNSIGNED NOT NULL DEFAULT 0', 'fk_images_en');
+  sql_create_field('arsenals', 'fk_formats', 'INT UNSIGNED NOT NULL DEFAULT 0', 'fk_images_fr');
   sql_create_field('arsenals', 'fk_arsenal_difficulties', 'INT UNSIGNED NOT NULL DEFAULT 0', 'fk_formats');
   sql_create_field('arsenals', 'is_hidden', 'TINYINT UNSIGNED NOT NULL DEFAULT 0', 'fk_arsenal_difficulties');
   sql_create_field('arsenals', 'name_en', 'TINYTEXT NOT NULL', 'is_hidden');
@@ -863,9 +865,13 @@ if($last_query < 13)
   sql_create_field('arsenals', 'gameplan_fr', 'LONGTEXT', 'gameplan_en');
   sql_create_field('arsenals', 'reserves_en', 'LONGTEXT', 'gameplan_fr');
   sql_create_field('arsenals', 'reserves_fr', 'LONGTEXT', 'reserves_en');
+  sql_create_field('arsenals', 'extra_en', 'LONGTEXT', 'reserves_fr');
+  sql_create_field('arsenals', 'extra_fr', 'LONGTEXT', 'extra_en');
 
   sql_create_index('arsenals', 'arsenals_uuid', 'uuid');
   sql_create_index('arsenals', 'arsenals_release', 'fk_releases');
+  sql_create_index('arsenals', 'arsenals_images_en', 'fk_images_en');
+  sql_create_index('arsenals', 'arsenals_images_fr', 'fk_images_fr');
   sql_create_index('arsenals', 'arsenals_format', 'fk_formats');
   sql_create_index('arsenals', 'arsenals_name_en', 'name_en(40)');
   sql_create_index('arsenals', 'arsenals_name_fr', 'name_fr(40)');
@@ -873,12 +879,17 @@ if($last_query < 13)
   sql_create_table('arsenals_compositions');
   sql_create_field('arsenals_compositions', 'fk_arsenals', 'INT UNSIGNED NOT NULL DEFAULT 0', 'id');
   sql_create_field('arsenals_compositions', 'fk_cards', 'INT UNSIGNED NOT NULL DEFAULT 0', 'fk_arsenals');
-  sql_create_field('arsenals_compositions', 'amount', 'INT UNSIGNED NOT NULL DEFAULT 0', 'fk_cards');
-  sql_create_field('arsenals_compositions', 'is_in_reserves', 'TINYINT UNSIGNED NOT NULL DEFAULT 0', 'amount');
+  sql_create_field('arsenals_compositions', 'amount_main', 'INT UNSIGNED NOT NULL DEFAULT 0', 'fk_cards');
+  sql_create_field('arsenals_compositions', 'amount_reserves', 'INT UNSIGNED NOT NULL DEFAULT 0', 'amount_main');
+  sql_create_field('arsenals_compositions', 'is_documentation', 'INT UNSIGNED NOT NULL DEFAULT 0', 'amount_reserves');
+  sql_create_field('arsenals_compositions', 'sorting_order', 'INT UNSIGNED NOT NULL DEFAULT 0', 'is_documentation');
 
   sql_create_index('arsenals_compositions', 'arsenals_compositions_arsenal', 'fk_arsenals');
   sql_create_index('arsenals_compositions', 'arsenals_compositions_card', 'fk_cards');
-  sql_create_index('arsenals_compositions', 'arsenals_compositions_reserves', 'is_in_reserves');
+  sql_create_index('arsenals_compositions', 'arsenals_compositions_main', 'amount_main');
+  sql_create_index('arsenals_compositions', 'arsenals_compositions_reserves', 'amount_reserves');
+  sql_create_index('arsenals_compositions', 'arsenals_compositions_documentation', 'is_documentation');
+  sql_create_index('arsenals_compositions', 'arsenals_compositions_sorting', 'sorting_order');
 
   sql_create_table('tags_arsenals');
   sql_create_field('tags_arsenals', 'fk_arsenals', 'INT UNSIGNED NOT NULL DEFAULT 0', 'id');
@@ -893,17 +904,6 @@ if($last_query < 13)
 
   sql_create_index('arsenals_factions', 'arsenals_factions_arsenal', 'fk_arsenals');
   sql_create_index('arsenals_factions', 'arsenals_factions_faction', 'fk_factions');
-
-  sql_create_table('arsenals_images');
-  sql_create_field('arsenals_images', 'fk_arsenals', 'INT UNSIGNED NOT NULL DEFAULT 0', 'id');
-  sql_create_field('arsenals_images', 'fk_images', 'INT UNSIGNED NOT NULL DEFAULT 0', 'fk_arsenals');
-  sql_create_field('arsenals_images', 'sorting_order', 'INT UNSIGNED NOT NULL DEFAULT 0', 'fk_images');
-  sql_create_field('arsenals_images', 'image_title_en', 'TINYTEXT NOT NULL', 'sorting_order');
-  sql_create_field('arsenals_images', 'image_title_fr', 'TINYTEXT NOT NULL', 'image_title_en');
-
-  sql_create_index('arsenals_images', 'arsenals_images_arsenal', 'fk_arsenals');
-  sql_create_index('arsenals_images', 'arsenals_images_image', 'fk_images');
-  sql_create_index('arsenals_images', 'arsenals_images_sorting', 'sorting_order');
 
   sql_create_table('formats');
   sql_create_field('formats', 'uuid', 'VARCHAR(36) NOT NULL', 'id');
