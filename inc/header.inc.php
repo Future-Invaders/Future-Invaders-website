b8343d48c8f62716d813ae4ab8fd7ba74b639d21
Éric Bisceglia <bisceglia.eric@gmail.com>
2024-11-23 15:59:16 +0100

    Social: Removed tournaments for now

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -397,9 +397,6 @@ $javascripts .= '
             <?=__link('pages/social/irc', __('submenu_social_irc'), 'header_submenu_link', 1, $path);?>
           </div>
           <div class="header_submenu_item">
-            <?=__link('404', __('submenu_social_tournaments'), 'header_submenu_wip', 1, $path);?>
-          </div>
-          <div class="header_submenu_item">
             <?=__link('pages/social/help', __('submenu_social_contribute'), 'header_submenu_link', 1, $path);?>
           </div>
         </div>
